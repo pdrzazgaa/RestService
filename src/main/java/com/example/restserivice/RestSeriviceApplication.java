b57a6e4a76c7bb09a9bedef2c2afd05972172f35
Paulina Drzazga <paulina@halpress.team>
2023-05-08 18:58:20 +0200

    Initial commit

---
+++ src/main/java/com/example/restserivice/RestSeriviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.restserivice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestSeriviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestSeriviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/restserivice/RestSeriviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restserivice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestSeriviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
